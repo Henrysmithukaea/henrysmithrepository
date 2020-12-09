ff48589e32abab827eb065c5902edaca357a07d2
Henrysmithukaea <75367004+Henrysmithukaea@users.noreply.github.com>
2020-12-09 09:19:40 +0000

    pngs now have specified dimensions

--- Excel graph handler sheet to graph - for awe data (backup).py
+++ Excel graph handler sheet to graph - for awe data (backup).py
@@ -45,8 +45,8 @@ def plotYandreturnlegendvalues(i, sname, colnos):
 def plotandsave(titlestring,yv):
     plt.title(titlestring)
     plt.axis(ymin=yv)
-    plt.ylabel("Count")
-    plt.rcParams.update({"savefig.facecolor":  ('white'),})  			#defines labels for your graph.
+    plt.ylabel("Count") 			#defines labels for your graph.
+    plt.rcParams.update({"savefig.facecolor":  ('white'),"figure.figsize": (18.5, 10)})  			#big nice pngs
     plt.yscale("log") 				#logarithmic axis
     plt.savefig(titlestring, bbox_inches='tight') #this will throw a bunch of .png files into the folder specified in filepath, overwriting anything with the same name.
     plt.show()				#shows you the graph that was saved
